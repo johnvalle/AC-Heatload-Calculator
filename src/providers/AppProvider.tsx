d00560a95ffdad201369d62da0b11567ac2d0b2f
johnvalle <vallejohn.personal@gmail.com>
2024-05-30 15:05:44 +0800

    Add initial locales, translation provider init

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,8 +1,10 @@
 import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
+import { I18nextProvider } from 'react-i18next';
 
 import { ErrorFallback } from '@/components';
+import i18n from '@/i18n';
 
 type AppProviderProps = {
   children?: React.ReactNode;
@@ -10,10 +12,12 @@ type AppProviderProps = {
 
 export const AppProvider = ({ children }: AppProviderProps) => {
   return (
-    <MantineProvider>
-      <React.Suspense fallback={<p>Loading...</p>}>
-        <ErrorBoundary FallbackComponent={ErrorFallback}>{children}</ErrorBoundary>
-      </React.Suspense>
-    </MantineProvider>
+    <I18nextProvider i18n={i18n}>
+      <MantineProvider>
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <ErrorBoundary FallbackComponent={ErrorFallback}>{children}</ErrorBoundary>
+        </React.Suspense>
+      </MantineProvider>
+    </I18nextProvider>
   );
 };
